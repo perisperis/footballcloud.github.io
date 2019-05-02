9d55c9e52a4eb8be7ebb17543b10c5d68d70e09f
Garcia Fernandez <pedro.garcia@atos.net>
2019-05-02 15:39:53 +0200

    Updates

--- main.js
+++ main.js
@@ -39,7 +39,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var routes = [];
+var routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 var AppRoutingModule = /** @class */ (function () {
     function AppRoutingModule() {
     }
